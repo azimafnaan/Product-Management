616c1efd385554c8e8aa2d0240250a611cea46b5
azimafnaan <azim.afnaan@hotmail.com>
2022-01-17 11:50:37 +0600

    added Home Page

--- src/components/featuredInfo/FeaturedInfo.js
+++ src/components/featuredInfo/FeaturedInfo.js
@@ -1,4 +1,4 @@
-import { ArrowDownward } from '@mui/icons-material'
+import { ArrowDownward, ArrowUpward } from '@mui/icons-material'
 import React from 'react'
 import './FeaturedInfo.css'
 
@@ -12,10 +12,31 @@ export default function FeaturedInfo () {
                 </span>
                 <div className="featuredMoneyContainer">
                     <span className="featuredMoney">$2,415</span>
-                    <span className="featuredMoneyRate">-11.4 <ArrowDownward /> </span>
+                    <span className="featuredMoneyRate">-11.4 <ArrowDownward className="featuredIcon negative" /> </span>
+                </div>
+                <span className="featuredSub">Compared to Last Month</span>
+            </div>
+            <div className="featuredItem">
+                <span className="featuredTitle">
+                    Sales
+                </span>
+                <div className="featuredMoneyContainer">
+                    <span className="featuredMoney">$4,415</span>
+                    <span className="featuredMoneyRate">-1.4 <ArrowDownward className="featuredIcon negative" /> </span>
+                </div>
+                <span className="featuredSub">Compared to Last Month</span>
+            </div>
+            <div className="featuredItem">
+                <span className="featuredTitle">
+                    Cost
+                </span>
+                <div className="featuredMoneyContainer">
+                    <span className="featuredMoney">$2,215</span>
+                    <span className="featuredMoneyRate">+$2,226 <ArrowUpward className="featuredIcon" /> </span>
                 </div>
                 <span className="featuredSub">Compared to Last Month</span>
             </div>
         </div>
+
     )
 }
