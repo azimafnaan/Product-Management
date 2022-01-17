2455e274b8e9ec495c3f34190f0d31578d535d8d
azimafnaan <azim.afnaan@hotmail.com>
2022-01-17 12:44:51 +0600

    Added Chart

---
+++ src/dummydata.js
@@ -0,0 +1,63 @@
+export const userData = [
+    {
+        name: 'Jan',
+        "Active User": 4000,
+
+    },
+    {
+        name: 'Feb',
+        "Active User": 3000,
+
+    },
+    {
+        name: 'Mar',
+        "Active User": 5000,
+
+    },
+    {
+        name: 'Apr',
+        "Active User": 4000,
+
+    },
+    {
+        name: 'May',
+        "Active User": 3000,
+
+    },
+    {
+        name: 'Jun',
+        "Active User": 4000,
+
+    },
+    {
+        name: 'Jul',
+        "Active User": 2000,
+
+    },
+    {
+        name: 'Aug',
+        "Active User": 3000,
+
+    },
+    {
+        name: 'Sep',
+        "Active User": 1000,
+
+    },
+    {
+        name: 'Oct',
+        "Active User": 5000,
+
+    },
+    {
+        name: 'Nov',
+        "Active User": 3000,
+
+    },
+    {
+        name: 'Dec',
+        "Active User": 4000,
+
+    }
+
+];
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import Chart from '../../components/chart/Chart';
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
 import './Home.css';
+import { userData } from '../../../src/dummydata'
+
 export default function Home () {
     return (
         <div className="home">
             <FeaturedInfo></FeaturedInfo>
-        </div>
+            <Chart data={userData} title="User Analytics" dataKey="Active User" grid></Chart>
+
+        </div >
     )
 }
 
